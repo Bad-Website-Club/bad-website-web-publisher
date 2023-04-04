7d5d4b8a4a4f92016120e641de5dc5377368dfa5
Ramón Huidobro <hola@ramonh.dev>
2023-04-04 16:58:31 +0200

    Add all the things

---
+++ .vitepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'A bad website', // appended to all page titles
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+    sidebar: [{ text: 'About', link: '/about' }],
+  }
+
+}
